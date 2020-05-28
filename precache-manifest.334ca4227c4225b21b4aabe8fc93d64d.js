54c0cb4f890fb20dbec6753943691636e9e3df8f
askelej <askeltechno@hotmail.com>
2020-05-28 02:05:47 +0200

    Updates

---
+++ precache-manifest.334ca4227c4225b21b4aabe8fc93d64d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a74cf9bd1644bc3d3ca6078409d525",
+    "url": "/space-x-missions/index.html"
+  },
+  {
+    "revision": "1d440cf9a6aac4be0004",
+    "url": "/space-x-missions/static/js/2.309fe2f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/space-x-missions/static/js/2.309fe2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de673ee7123beeb2e195",
+    "url": "/space-x-missions/static/js/main.05ba9482.chunk.js"
+  },
+  {
+    "revision": "236efa7e3afbc8a4f1e0",
+    "url": "/space-x-missions/static/js/runtime-main.4b5c01f4.js"
+  }
+]);
